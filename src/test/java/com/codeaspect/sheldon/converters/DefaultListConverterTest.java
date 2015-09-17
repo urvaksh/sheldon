2c6fbdd5bb02770d9b3c077b9448062c401c0aa2
urvaksh <urvaksh.rogers@gmail.com>
2015-09-17 18:52:07 +0530

    Simple cleanup

--- src/test/java/com/codeaspect/sheldon/converters/DefaultListConverterTest.java
+++ src/test/java/com/codeaspect/sheldon/converters/DefaultListConverterTest.java
@@ -19,11 +19,7 @@ public class DefaultListConverterTest {
 		final Set<Integer> integers = new HashSet<Integer>(Arrays.asList(1,2,3,4,5,6));
 		
 		@SuppressWarnings("unchecked")
-		List<Integer> ints = (List<Integer>)converter.convertToList(null, new Iterable<Integer>(){
-	          public Iterator<Integer> iterator() {
-	              return integers.iterator();
-	          }
-	      });
+		List<Integer> ints = (List<Integer>) converter.convertToList(null, integers);
 		
 		for(Integer i : ints){
 			assertTrue(integers.contains(i));
